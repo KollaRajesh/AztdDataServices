dca48107c751102fd86b191ffb2294969077c76e
Rajesh Kolla <razesh.kolla@gmail.com>
2020-12-21 18:34:29 -0500

    removed startup.cs and ServiceProviderFactory.cs

--- Az-SQL/Demo.EFCore.AzSQL/Program.cs
+++ Az-SQL/Demo.EFCore.AzSQL/Program.cs
@@ -1,45 +1,81 @@
 ﻿using System;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Hosting;
-using System.Linq;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Demo.EFCore.AzSQL.Models;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System.Reflection;
+using Microsoft.AspNetCore.Hosting.Internal;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Demo.EFCore.AzSQL
 {
-   public  class Program
+    public  class Program
     {
-        public static void Main(string[] args)
+        private const string _prefix="";
+        private const string _appsettings = "appsettings.json";
+        private const string _hostsettings = "hostsettings.json";
+        public static async Task  Main(string[] args)
         {
                 
-        // var configuration =  ConfigureBuilder().Build();
-        // IServiceCollection services = new ServiceCollection();
-        // Startup startup = new Startup(configuration);
-        // startup.ConfigureServices(services);
-        // IServiceProvider serviceProvider = services.BuildServiceProvider();
-        
-        using (var context= ServiceProviderFactory.ServiceProvider.GetService<sqldbaztdContext>()){
+            var HostBuilder =  CreateHostBuilder(args).Build();
+          var services= HostBuilder.Services.CreateScope();
+          var  service =services.ServiceProvider.GetService<Startup>();
+          using (var context= services.ServiceProvider.GetService<sqldbaztdContext>())
+          {
             foreach (var product in context.Products)
             {
                 Console.WriteLine("Product Name : {0} and price :{1}" , product.Name, product.ListPrice);
             }
 
-        }
+          }
+
+        await HostBuilder.RunAsync();
 
         }
 
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
+             Host.CreateDefaultBuilder(args)
+             .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ??"Development")
+             .ConfigureHostConfiguration(configHost => 
+             {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile(_hostsettings, optional: true);
+                    configHost.AddEnvironmentVariables();
+                    configHost.AddCommandLine(args);
+             })
+             .ConfigureAppConfiguration((hostContext, configApp)=>{
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile(_appsettings, optional: true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                                            optional: true);
+                    configApp.AddEnvironmentVariables(prefix: _prefix);
+                    if (hostContext.HostingEnvironment.IsDevelopment()){
+                    configApp.AddUserSecrets<Program>();
+                    }
+                    configApp.AddCommandLine(args);
+                    
 
+                }
+             )
+             .ConfigureServices((HostContext,services)=> 
+             {
+                services.AddLogging();
+                var database=HostContext.Configuration.GetConnectionString("sqldbDatabase");
+                services.AddDbContext<sqldbaztdContext>(options =>
+                options.UseSqlServer(database));
+                services.AddTransient<Startup>();
 
-        //  public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //      Host.CreateDefaultBuilder(args)
-        //     .ConfigureWebHostDefaults(webBuilder =>
-        //     {
-        //         webBuilder.UseStartup<Startup>();
-        //     });
+             });
+ 
+            
+
+            // .ConfigureWebHostDefaults(webBuilder =>
+            // {
+            //     webBuilder.UseStartup<Startup>();
+            // }
+            // );
+
+            
     }
 }
